7d09794422a80642f27becfcbfcff9b10e66cedb
andrey <andreytorrente@gmail.com>
2023-01-20 11:51:47 -0600

    feat: add state to context to know if user is logged or registered

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,10 @@ import "./sass/main.scss";
 function App() {
 
   const [isRegistering, setIsRegistering] = useState(true);
+  const [isUserReady, setIsUserReady] = useState(false);
 
   return (
-    <Context.Provider value ={{isRegistering, setIsRegistering}}>
+    <Context.Provider value ={{isRegistering, setIsRegistering,isUserReady, setIsUserReady}}>
       <div className="App">
         <Inicio></Inicio>
       </div>
