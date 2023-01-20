0ff043c681476f1035980008a043cd876bed096a
andrey <andreytorrente@gmail.com>
2023-01-20 12:31:49 -0600

    feat: stablished basic UI functionality to logout

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,20 @@
+import { useContext } from "react";
+import { Context } from "../context/Context";
 import Logo from "./Logo";
 const Navbar = () => {
+  const {setIsUserReady} = useContext(Context)
+
+  const handleLogOut = ()=>{
+    if(confirm("Realmente desea salir?")){
+      setIsUserReady(false)
+    }
+  }
   return <nav className="nav">
     <Logo></Logo>
+    
     <ul className="links">
         <li><a href="#">Historial</a></li>
-        <li><a href="#">Salir</a></li>
+        <li><a href="#" onClick={handleLogOut}>Salir</a></li>
     </ul>
     </nav>;
 };
