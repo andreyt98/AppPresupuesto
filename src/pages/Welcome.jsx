a94690ff8c396ad4969dccfb3bdb03820b20766f
andrey <andreytorrente@gmail.com>
2023-01-20 11:57:34 -0600

    style: rename component

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import Form from "../Components/Form";
+import Logo from "../Components/Logo";
+
+const Welcome = () => {
+  return (
+    <section className="inicio">
+      <Logo></Logo>
+      <Form></Form>
+    </section>
+  );
+};
+
+export default Welcome;
