dac9e4634a08b14a4a26c6330be6f22a22bc99d1
andrey <andreytorrente@gmail.com>
2023-01-21 18:33:36 -0600

    feat: add message component to context to shw errors and alerts

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ import "./sass/main.scss";
 function App() {
   const [isRegistering, setIsRegistering] = useState(true);
   const [isUserReady, setIsUserReady] = useState(false);
+  const [message, setMessage] = useState({active:false,text:null});
 
   return (
-    <Context.Provider value={{ isRegistering, setIsRegistering, isUserReady, setIsUserReady }}>
+    <Context.Provider value={{ isRegistering, setIsRegistering, isUserReady, setIsUserReady,message,setMessage }}>
       <div className="App">{isUserReady ? <UserHome></UserHome> : <Welcome></Welcome>}</div>
     </Context.Provider>
   );
