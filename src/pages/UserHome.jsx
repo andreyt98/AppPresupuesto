6ebf054169178ae00ca92ca869778a526417dbad
andrey <andreytorrente@gmail.com>
2023-01-28 18:42:45 -0600

    feat: add budget form, budget card and expense form to user home page

--- src/pages/UserHome.jsx
+++ src/pages/UserHome.jsx
@@ -1,13 +1,25 @@
 import { useContext } from "react";
+import BudgetCard from "../Components/BudgetCard";
+import BudgetForm from "../Components/BudgetForm";
+import ExpenseForm from "../Components/ExpenseForm";
 import Navbar from "../Components/Navbar";
 import { Context } from "../context/Context";
-
 const UserHome = () => {
-  const {user} = useContext(Context)
-  return <div>
-    <Navbar></Navbar>
-    hola! {user.name}
-    </div>;
+  const { user, budget } = useContext(Context);
+
+  return (
+    <section className="user-home">
+      <Navbar></Navbar>
+      {budget.ready || <BudgetForm></BudgetForm>}
+
+      {budget.ready && (
+        <>
+          <BudgetCard></BudgetCard>
+          <ExpenseForm></ExpenseForm>
+        </>
+      )}
+    </section>
+  );
 };
 
 export default UserHome;
